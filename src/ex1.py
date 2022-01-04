a73137b82a980a1fb780812c2a6a5d06979baafa
Pieter van der Deijl <p.c.vanderdeijl@gmail.com>
2022-01-04 15:58:39 +0100

    fist excercise

---
+++ src/ex1.py
@@ -0,0 +1,22 @@
+file = open('../data/ex1.txt', 'r')
+lines = file.readlines()
+nums = [int(x) for x in lines]
+
+stop = False
+for x in nums:
+    if(stop):
+        break
+    for y in nums:
+        if(x == y):
+            continue
+        if(stop):
+            break
+        for z in nums:
+            
+            if(stop):
+                break
+            if(z == y or z == x):
+                continue
+            if(x+y+z == 2020):
+                print(x*y*z)
+                stop = True
