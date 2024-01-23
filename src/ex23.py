7a5d328ba21ae35cdb575e2f77abd4b0874c42df
p.c.van.der.deijl@ind.nl <p.c.van.der.deijl@ind.nl>
2024-01-23 21:35:32 +0100

    ex23b

--- src/ex23.py
+++ src/ex23.py
@@ -1,78 +1,140 @@
+from pycharm_line_profiler import profile
+
 def in_range(_first, _last, _destination):
     if _first < _last:
         return _first <= _destination < _last
     else:
         return _destination >= _first or _destination < _last
 
-destinations = []
-currents = []
-firsts = []
-lasts = []
 
-cups = '716892543'
-current = 0
-len_cups = len(cups)
-for m in range(1, 101):
-    for c in cups:
-        if cups.count(c) > 1:
-            print("ERROR", cups)
-            exit()
-    if len(cups) != len_cups:
-        print("ERROR", cups)
-        exit()
-    current_label = cups[current]
-    destination = cups.find(str(int(current_label)-1))
-    if destination == -1:
-        destination = cups.find('9')
-    first = (current + 1) % len(cups)
-    last = (current + 4) % len(cups)
-    destination_label = cups[destination]
 
-    while in_range(first, last, destination):
-        destination_label = int(cups[destination])
-        destination = cups.find(str(destination_label - 1))
-        destination_label = destination_label - 1
-        if destination == -1:
-            if destination_label == 0:
-                destination_label = 9
-                destination = cups.find(str(destination_label))
-            else:
-                print("ERROR, strange destination situation", destination, destination_label)
-                exit()
-    destinations.append(destination)
-    if last > first:
-        pick_up = cups[first:last]
-        if destination >= last:
-            cups = cups.replace(pick_up, '')
-            destination = cups.find(str(destination_label))
-            cups = cups[:destination+1] + pick_up + cups[destination+1:]
-        elif destination < first:
-            cups = cups[0:destination+1] + pick_up + cups[destination+1:first] + cups[last:]
-        else:
-            print("ERROR: destination is in cups")
+def do_run(pt2, test):
+    debug_freq = 100
+    size = 9
+
+    if test:
+        size = 50
+        input = '389125467'
+        debug_freq = 1
+
     else:
-        pick_up = cups[first:] + cups[:last]
-        # if destination >= last:
-        #     cups = cups[last:destination+1] + pick_up + cups[:first]
-        # else:
-        cups = cups[last:destination+1] + pick_up + cups[destination+1:first]
+        input = '716892543'
+        n_rounds = 100
+        largest = 4
 
-    current = (cups.find(current_label)+1) % len(cups)
-    print(f"pickup: {pick_up}")
-    print(f"destination: {destination_label}")
-    print()
-    currents.append(current)
-    firsts.append(first)
-    lasts.append(last)
+    cups = [int(n) for n in list(input)]
+    if pt2:
+        size = 1000000
 
-pivot = cups.find('1')
-if pivot == len(cups)-1:
-    final_cups = cups.replace('1', '')
-else:
-    final_cups = cups[pivot+1:] + cups[:pivot]
-print(currents)
-print(destinations)
-print(firsts)
-print(lasts)
-print(final_cups)
+    if pt2 or test:
+        extra_cups = list(range(10, size+1))
+        cups += extra_cups
+        largest = size - 1
+        n_rounds = size*10
 
+    current = 0
+    len_cups = len(cups)
+    if test or not pt2:
+        print(cups)
+    for m in range(1, n_rounds+1):
+        if test:
+            if largest != cups.index(size):
+                print("ERROR: position of largest not correct", largest, cups.index(size), '\n', cups)
+                exit()
+
+        current_label = cups[current]
+        destination_label = current_label-1
+        if destination_label == 0:
+            destination_label = size
+            destination = largest
+        else:
+            destination = cups.index(destination_label)
+        first = (current + 1) % len(cups)
+        last = (current + 4) % len(cups)
+        while in_range(first, last, destination):
+            destination_label -= 1
+            if destination_label == 0:
+                destination = largest
+                destination_label = size
+            else:
+                destination = cups.index(destination_label)
+
+        if last > first:
+            pick_up = cups[first:last]
+            if destination >= last:
+                # cups = cups.replace(pick_up, '')
+                if largest < first or largest > destination:
+                    pass
+                elif largest >= last:
+                    largest -= 3
+                elif first <= largest < last:
+                    largest = destination - 3 + (largest - first) + 1
+                else:
+                    print("ERROR: position of largest not clear", largest, destination, first, last)
+                    exit()
+                cups = cups[:first] + cups[last:destination+1] + pick_up + cups[destination+1:]
+
+            elif destination < first:
+                if largest <= destination or largest >= last:
+                    pass
+                elif destination < largest < first:
+                    largest += 3
+                elif first <= largest < last:
+                    largest -= (first - destination) - 1
+                else:
+                    print("ERROR: position of largest not clear 2", largest, destination, first, last)
+                    exit()
+                cups = cups[:destination+1] + pick_up + cups[destination+1:first] + cups[last:]
+
+            else:
+                print("ERROR: destination is in cups")
+                exit()
+        else:
+            pick_up = cups[first:] + cups[:last]
+            # if destination >= last:
+            #     cups = cups[last:destination+1] + pick_up + cups[:first]
+            # else:
+            if largest >= first:
+                largest = destination + (largest - first) - last + 1
+            elif largest < last:
+                largest = destination + (size-first) + 1 - last + largest
+            elif largest <= destination:
+                largest -= last
+            elif largest > destination:
+                largest += (size-first)
+            else:
+                print("ERROR: position of largest not clear 3", largest, destination, first, last)
+                exit()
+            cups = cups[last:destination+1] + pick_up + cups[destination+1:first]
+
+        largest = largest % len(cups)
+        current = (cups.index(current_label)+1) % len(cups)
+        if (m % debug_freq) == 0:
+            print(f"pickup: {pick_up}")
+            print(f"current: {current}")
+            print(f"current label: {current_label}")
+
+            print(f"destination: {destination}")
+            print(f"destination label: {destination_label}")
+            print(f"first: {first}")
+            if test:
+                print(cups)
+            print(f"round: {m} \n")
+
+
+    pivot = cups.index(1)
+    if pivot == len(cups)-1:
+        final_cups = cups[:-1]
+    else:
+        final_cups = cups[pivot+1:] + cups[:pivot]
+
+    # print(currents)
+    # print(destinations)
+    # print(current_labels)
+    # print(destination_labels)
+    # print(firsts)
+    # print(lasts)
+    print(final_cups)
+
+if __name__ == '__main__':
+    do_run(False, True)
